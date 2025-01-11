cf640fe07bf8cb358b81d85ad91d05febc2393f6
lboulanger <louis.boulanger.work@gmail.com>
2025-01-11 11:56:10 +0100

    fix alpinejs with cdn

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
-import Alpine from '../../node_modules/alpinejs/dist/module.esm.js';
+// import Alpine from '../../node_modules/alpinejs/dist/module.esm.js';
+import { Alpine } from 'https://cdn.jsdelivr.net/npm/alpinejs@3.14.7/+esm';
 import Testimonials from "./components/testimonials.js";
 
 window.addEventListener('DOMContentLoaded', () => {
